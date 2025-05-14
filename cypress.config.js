6795a9e8d7ef0c5641b5854c6e9cf9d610f6fa00
Katya <evil.666.buny@gmail.com>
2025-05-14 17:04:37 +0300

    tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,13 @@
+/* eslint-disable max-len */
 const { defineConfig } = require('cypress');
+const { addMatchImageSnapshotPlugin } = require('@emerson-eps/cypress-image-snapshot/plugin');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: `https://react-redux.realworld.io`,
     setupNodeEvents(on, config) {
+      addMatchImageSnapshotPlugin(on, config);
+      return config;
     }
   }
 });
--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,29 @@
-/// <reference types="cypress" />
+/* eslint-disable no-lone-blocks */
+// import { faker } from '@faker-js/faker';
+
+// eslint-disable-next-line block-spacing
+{
+  /* <reference types="cypress" />; */
+}
+
+const { createRandomUser } = require('../support/commands.js');
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    const { username, email, password } = createRandomUser();
+    cy.visit('https://react-redux.realworld.io');
+    cy.contains('Sign up').click();
+    cy.get('input[placeholder="Email"]').type(email);
+    cy.get('input[placeholder="Password"]').type(password);
+    cy.get('input[placeholder="Username"]').type(username);
+    cy.get('button').contains('OK').click();
+    cy.contains('Settings').click();
+    cy.contains('log out').click();
 
+    cy.contains('Sign in').click();
+    cy.get('input[type="email"]').type(email);
+    cy.get('input[type="password"]').type(password);
+    cy.get('button').contains('Sign in').click();
+    cy.get('nav').contains(username).should('be.visible');
   });
 });
